6d1e848ceca0b5801386af32795c9ec5122be4df
DongHun Lee <ldh960102@gmail.com>
2020-07-06 15:29:23 +0900

    EL practice is added

--- JSP_Project/WebContent/spaghetti.jsp
+++ JSP_Project/WebContent/spaghetti.jsp
@@ -8,7 +8,19 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
+
+<%
+	pageContext.setAttribute("result", "hi");
+%>
+
 <body>
 	<%=request.getAttribute("result") %> number.
+	${requestScope.result} <br>
+	${names[0]} <br>
+	${notice.title}<br>
+	${result} <br>
+	${param.n ge 3}<br>
+	${empty param.n ? 'this is empty_value':param.n}<br>
+	${header.accept}
 </body>
 </html>
--- JSP_Project/src/kr/mycom/web/add/Spaghetti.java
+++ JSP_Project/src/kr/mycom/web/add/Spaghetti.java
@@ -1,6 +1,7 @@
 package kr.mycom.web.add;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sun.javafx.collections.MappingChange.Map;
 
 @WebServlet("/spag")
 public class Spaghetti extends HttpServlet{
@@ -28,7 +31,16 @@ public class Spaghetti extends HttpServlet{
 			result = "even";
 		}
 			
-		request.setAttribute("result", result);	
+		request.setAttribute("result", result);
+		
+		String[] names = {"lee", "Dh"};
+		request.setAttribute("names", names);
+		
+		HashMap<String, Object> notice = new HashMap<String, Object>();
+		notice.put("id",1);
+		notice.put("title", "This is EL");
+		request.setAttribute("notice", notice);
+		
 		//redirect
 		//forward
 		RequestDispatcher dispatcher = request.getRequestDispatcher("spaghetti.jsp");
